b30d2e7803f1dbb075dada734825753de667438f
Antti Rautiainen <Antti.rautiainen@gmail.com>
2015-02-13 07:06:06 +0200

    sequencediagram

--- StarMap/src/main/java/starmap/Main.java
+++ StarMap/src/main/java/starmap/Main.java
@@ -5,61 +5,18 @@
  */
 package starmap;
 
-import com.joml.matrix.Matrix3f;
-import com.joml.vector.Vector3f;
-import java.io.FileNotFoundException;
+//import com.joml.matrix.Matrix3f;
+//import com.joml.vector.Vector3f;
+//import java.io.FileNotFoundException;
 
 /**
  *
  * @author air
- */
+*/
+
 public class Main {
     public static void main(String[] args) {
-        
-        Vector3f testcenter = new Vector3f(-50,-50,0);
-        Matrix3f testtransformation = new Matrix3f();
-        testtransformation.identity();
-        testtransformation.mul((float) 100);
-        StarCube testcube = new StarCube(testcenter,testtransformation); 
-        System.out.print(testcube); 
-        Starset Otava = new Starset(testcube, "Dubhe", "Merak", "Phad", "Megrez", "Alioth", "Mizar", "Alkaid");
-        Otava.print();
-        /*
-        double width = 0;
-        double newwidth;
-        for (Star star1 : Otava.getStars()) {
-            for (Star star2: Otava.getStars()) {
-                newwidth = Math.abs(star1.getRa() - star2.getRa());
-                if (newwidth > width) {
-                    width = newwidth;
-                }
-            }
+        Starset bigDipper = new Starset();
+        new StarFrame().execute(bigDipper.getgLoopObject());
         }
-                
-        System.out.print((360.0/24.0) * width);
-        */
-        double sumx = 0, sumy = 0, sumz = 0;
-        for (Star star1 : Otava.getStars()) {
-                sumx += star1.getX();
-                sumy += star1.getY();
-                sumz += star1.getZ();
-                System.out.println(star1.getProperName());
-                System.out.println(star1.getDistance());
-        }
-           
-        int n = Otava.getStars().size();
-        Vector3f testcenter2 = new Vector3f((float) sumx / n, (float) sumy / n, (float) sumz / n);
-        
-        StarCube testcube2 = new StarCube(testcenter2,(float) (50 * (Math.PI/180))); 
-        System.out.print(testcube2);
-        Starset Otava2 = new Starset(testcube2, "Dubhe", "Merak", "Phad", "Megrez", "Alioth", "Mizar", "Alkaid");
-                
-        // new StarFrame().show(Otava);
- 
-        //new StarFrame().execute(Otava.getgLoopObject());
-        
-        new StarFrame().execute(Otava2.getgLoopObject());
-     
-        }
-    }
-
+}
--- StarMap/src/main/java/starmap/Starset.java
+++ StarMap/src/main/java/starmap/Starset.java
@@ -1,6 +1,8 @@
 package starmap;
 
 import Lwjglwrapper.GLoopObject;
+import com.joml.matrix.Matrix3f;
+import com.joml.vector.Vector3f;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,7 @@ public class Starset {
     
     public Starset(StarCube starcube, String... args) {
         gLoopObject = new StarGLoop(this, starcube);
-        stars = new ArrayList<>();
+        stars = new ArrayList<Star>();
         
         File csvStardata = new File("hygdata_v3.csv");
         //File csvStardata = new File("hygsmalldata.csv");
@@ -48,6 +50,56 @@ public class Starset {
         
     }
 
+    /**
+    * Starset by default is scaled to center of Big dipper (Otava), 50 degrees slice of sky
+    */
+    public Starset() {
+        stars = new ArrayList<Star>();
+        
+        String args[] = {"Dubhe", "Merak", "Phad", "Megrez", "Alioth", "Mizar", "Alkaid"};
+      
+        File csvStardata = new File("hygdata_v3.csv");
+        //File csvStardata = new File("hygsmalldata.csv");
+        
+        Star star;
+        Scanner scanner = null;
+        String csvString;
+        
+        try {
+            scanner = new Scanner(csvStardata);
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+        
+        csvString = scanner.nextLine(); 
+                
+        while (scanner.hasNextLine()) {
+            csvString = scanner.nextLine();
+            star = new Star(csvString);
+            for (String arg : args) {
+                if (arg.equals(star.getProperName())) {
+                stars.add(star);
+                }
+            }
+        }
+        
+        double sumx = 0, sumy = 0, sumz = 0;
+        for (Star star1 : stars) {
+                sumx += star1.getX();
+                sumy += star1.getY();
+                sumz += star1.getZ();
+        }
+           
+        int n = stars.size();
+        Vector3f bigDipperCenter = new Vector3f((float) sumx / n, (float) sumy / n, (float) sumz / n);
+        
+        StarCube starcube = new StarCube(bigDipperCenter,(float) (50 * (Math.PI/180))); 
+        gLoopObject = new StarGLoop(this, starcube);
+        
+    }
+    
+
+    
     public GLoopObject getgLoopObject() {
         return gLoopObject;
     }
